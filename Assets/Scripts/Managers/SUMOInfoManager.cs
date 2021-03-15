8848237486b3105e7ce8f980aecd96fb00f9e6ac
XuanpengZhao <64811969+XuanpengZhao@users.noreply.github.com>
2021-03-15 12:13:02 -0700

    Managed info from SUMO

---
+++ Assets/Scripts/Managers/SUMOInfoManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+/*
+ * Get postion from SUMO
+ * Assign ID and pos
+ */
+public class SUMOInfoManager : MonoBehaviour
+{
+
+
+    private void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+       
+    }
+}
